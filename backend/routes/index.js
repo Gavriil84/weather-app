05b0f622c2ac161ebe6820c0f19bb0f829f43334
Gavriil84 <gabwt.84@gmail.com>
2022-03-24 20:28:28 -0700

    update index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/profile', (req, res) => {
-    res.status(200).json({ bitch: "slut" })
+    res.status(200).json({ message: "hello" })
 })
 
 
