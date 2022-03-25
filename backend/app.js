c5122a12168c4effe286e7b745c1f57063e312ef
Gavriil84 <gabwt.84@gmail.com>
2022-03-24 19:56:11 -0700

    backend config

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,21 @@
-console.log("helloooooooo")
+const express = require('express');
+const dotenv = require('dotenv').config;
+const port = 5000;
+const routes = require('./routes/index');
+const connectDB = require('./config/connect');
+
+connectDB
+    .then(() => {
+        console.log("connected to Database!");
+        app.listen(port, () => console.log(`Running app on PORT:${port}`))
+    });
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.use('/api/v1', routes)
