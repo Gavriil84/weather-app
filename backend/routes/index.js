a5d558a2a5a15ab61f21882e9c2c8247327040ab
Gavriil84 <gabwt.84@gmail.com>
2022-03-30 14:23:54 -0700

    update router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+const profileRouter = require('./profiles');
 
-router.get('/profile', (req, res) => {
-    res.status(200).json({ message: "hello" })
-})
+// router.get('/profile', (req, res) => {
+//     res.status(200).json({ message: "hello" })
+// })
+
+router.use('/api/v1/profile', profileRouter);
 
 
 module.exports = router;
