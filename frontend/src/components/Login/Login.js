44e6a5c6656ed29521efed28414a80253778eb73
Gavriil84 <gabwt.84@gmail.com>
2022-04-02 00:46:21 -0700

    create login component

---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Button from '../../shared/Button'
+
+import './Login.scss';
+
+const Login = () => {
+    return (
+        <div className="login">
+            <h1>Log In</h1>
+            <form>
+                <label>
+                    <p>Email</p>
+                    <input type="email" />
+                </label>
+
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+
+                <span>Don't have an account? <button>Create account</button></span>
+                <Button type='submit'>Log In</Button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
