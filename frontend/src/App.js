3ce05d7d875888c970ed66d257bb8495bf6c6ccb
Gavriil84 <gabwt.84@gmail.com>
2022-04-03 22:20:48 -0700

    setup login router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import './App.scss';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Profile from './components/Profile/Profile';
+import Signup from './components/Sigup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        {/* <Link to="/login">Login</Link> */}
+        {/* Header comes here */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Home />} />
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
