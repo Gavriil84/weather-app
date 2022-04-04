00e606860ea22791a1dd814063dfbe3bb93a47ce
Gavriil84 <gabwt.84@gmail.com>
2022-04-03 22:20:57 -0700

    implement login function

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,21 +1,65 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import Button from '../../shared/Button'
 
 import './Login.scss';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [err, setErr] = useState(false);
+
+    const login = async (e) => {
+        e.preventDefault();
+
+        try {
+            let config = {
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    "Access-Control-Allow-Origin": "*",
+                }
+            }
+
+            const { data } = await axios.post('http://localhost:5000/api/v1/login', { email, password }, config)
+
+            if (email == data.email && password == data.password) {
+                localStorage.setItem('userInfo', JSON.stringify(data.id))
+                setErr(false)
+                setEmail('')
+                setPassword('')
+            } else {
+                setErr(true)
+                console.log(data)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const emailHandler = (e) => {
+        e.preventDefault();
+
+        setEmail(e.target.value)
+    }
+
+    const passwordHandler = (e) => {
+        e.preventDefault();
+
+        setPassword(e.target.value)
+    }
+
     return (
         <div className="login">
             <h1>Log In</h1>
-            <form>
+            <form onSubmit={login}>
                 <label>
                     <p>Email</p>
-                    <input type="email" />
+                    <input type="email" name="email" value={email} onChange={emailHandler} />
                 </label>
 
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" name="password" value={password} onChange={passwordHandler} />
                 </label>
 
                 <span>Don't have an account? <button>Create account</button></span>
