c2ba03abbf0c13b0a0cbaaeaa92ee87caa129914
Gavriil84 <gabwt.84@gmail.com>
2022-04-08 15:57:14 -0700

    add validator

--- backend/routes/profiles.js
+++ backend/routes/profiles.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const validators = require('../validator');
 
 const { getProfiles, getProfile, postProfile, putProfile } = require('../controller/profileController')
 
 router.get("/", getProfiles)
 router.get("/:id", getProfile)
-router.post("/", postProfile)
+router.post("/", validators.signupValidator, postProfile)
 router.put("/:id", putProfile)
 
 module.exports = router;
