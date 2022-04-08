df86fc88acfb6816e9e9b363883ad5c6b194de8c
Gavriil84 <gabwt.84@gmail.com>
2022-04-08 16:54:32 -0700

    update url

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -45,7 +45,7 @@ const Home = () => {
             favorite: favoriteArr
         }
 
-        axios.put(`https://wmdd4936-gwatanabe00.herokuapp.com/api/v1/profile/${userId}`, favoriteLocation)
+        axios.put(`/api/v1/profile/${userId}`, favoriteLocation)
     }
 
     const debouncedSave = useCallback(
